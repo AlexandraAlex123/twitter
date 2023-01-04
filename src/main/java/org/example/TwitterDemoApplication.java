9201c835c37e4c1d21d7060d7282056b468f2d2e
AlexandraAlex123 <110481625+AlexandraAlex123@users.noreply.github.com>
2023-01-04 09:06:34 +0200

    TwitterApp

--- src/main/java/org/example/TwitterDemoApplication.java
+++ src/main/java/org/example/TwitterDemoApplication.java
@@ -4,7 +4,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TwitterDemoApplication {
 
